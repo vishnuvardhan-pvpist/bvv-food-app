e54b7a4172453b4feffa764721a06bf45e8ba0c4
gracyaneoliveira <gracyane_oliveira@hotmail.com>
2021-01-05 20:06:52 -0300

    GET /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -33,7 +33,7 @@ public class Order implements Serializable{
 	@JoinTable(name="tb_order_product", 
 		joinColumns = @JoinColumn(name="order_id"),
 		inverseJoinColumns = @JoinColumn(name="product_id"))
-	private Set<Product> produtcs = new HashSet<>();
+	private Set<Product> products = new HashSet<>();
 	
 	public Order() {}
 
@@ -95,8 +95,8 @@ public class Order implements Serializable{
 		this.status = status;
 	}
 
-	public Set<Product> getProdutcs() {
-		return produtcs;
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	@Override
